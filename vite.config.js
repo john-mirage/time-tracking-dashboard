b18c093b34cc53e0e2f437636c2c63bb521bb15e
john-mirage <chaosdynamix@posteo.net>
2022-05-02 10:40:43 +0200

    feat: initial commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/time-tracking-dashboard/",
+  resolve: {
+    alias: {
+      '@images': resolve(__dirname, 'src/images'),
+      '@styles': resolve(__dirname, 'src/styles'),
+    },
+  },
+})
