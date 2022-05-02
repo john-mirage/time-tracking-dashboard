45a9a74d2cf106c92d5f5f25400eda1134ad1dc1
john-mirage <chaosdynamix@posteo.net>
2022-05-02 15:27:26 +0200

    feat: add logic

--- src/data/stats.ts
+++ src/data/stats.ts
@@ -5,107 +5,133 @@ import exerciseIcon from "@images/icon-exercise.svg";
 import socialIcon from "@images/icon-social.svg";
 import selfCareIcon from "@images/icon-self-care.svg";
 
-const stats = [
+interface Period {
+  name: string;
+  current: number;
+  previous: number;
+}
+
+interface Stat {
+  name: string;
+  daily: Period;
+  weekly: Period;
+  monthly: Period;
+  image: string;
+}
+
+const stats: Stat[] = [
   {
-    name: "Work",
+    name: "work",
     daily: {
+      name: "day",
       current: 7,
       previous: 6,
     },
     weekly: {
+      name: "week",
       current: 32,
       previous: 36,
     },
     monthly: {
+      name: "month",
       current: 120,
       previous: 125,
     },
-    color: "work",
     image: workIcon,
   },
   {
-    name: "Play",
+    name: "play",
     daily: {
+      name: "day",
       current: 1,
       previous: 2,
     },
     weekly: {
+      name: "week",
       current: 10,
       previous: 8,
     },
     monthly: {
+      name: "month",
       current: 30,
       previous: 42,
     },
-    color: "play",
     image: playIcon,
   },
   {
-    name: "Study",
+    name: "study",
     daily: {
+      name: "day",
       current: 1,
       previous: 2,
     },
     weekly: {
+      name: "week",
       current: 4,
       previous: 7,
     },
     monthly: {
+      name: "month",
       current: 12,
       previous: 16,
     },
-    color: "study",
     image: studyIcon,
   },
   {
-    name: "Exercise",
+    name: "exercise",
     daily: {
+      name: "day",
       current: 1,
       previous: 2,
     },
     weekly: {
+      name: "week",
       current: 4,
       previous: 5,
     },
     monthly: {
+      name: "month",
       current: 16,
       previous: 20,
     },
-    color: "exercise",
     image: exerciseIcon,
   },
   {
-    name: "Social",
+    name: "social",
     daily: {
+      name: "day",
       current: 1,
       previous: 2,
     },
     weekly: {
+      name: "week",
       current: 5,
       previous: 10,
     },
     monthly: {
+      name: "month",
       current: 20,
       previous: 21,
     },
-    color: "social",
     image: socialIcon,
   },
   {
-    name: "Self Care",
+    name: "self care",
     daily: {
+      name: "day",
       current: 0,
       previous: 1,
     },
     weekly: {
+      name: "week",
       current: 2,
       previous: 2,
     },
     monthly: {
+      name: "month",
       current: 8,
       previous: 9,
     },
-    color: "self-care",
     image: selfCareIcon,
   },
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
 import './main.css';
+import stats from "@data/stats";
+
+const inputs = document.querySelectorAll(".menu__input") as NodeListOf<HTMLInputElement>;
+const grid = document.querySelector("#grid") as HTMLDivElement;
+const template = document.querySelector("#template") as HTMLTemplateElement;
+
+inputs.forEach((input) => {
+  input.addEventListener("change", (event) => {
+    document.dispatchEvent(new CustomEvent("change-period", {
+      detail: event.target.value,
+    }));
+  });
+});
+
+stats.forEach((stat) => {
+  const statFragment = template.content.cloneNode(true) as HTMLElement;
+  const title = statFragment.querySelector(".stat__title") as HTMLHeadingElement;
+  const currentValue = statFragment.querySelector(".stat__current-value") as HTMLSpanElement;
+  const previousValue = statFragment.querySelector(".stat__previous-value") as HTMLSpanElement;
+  const previousPeriod = statFragment.querySelector(".stat__previous-period") as HTMLSpanElement;
+  title.textContent = stat.name;
+  currentValue.textContent = String(stat.weekly.current);
+  previousValue.textContent = String(stat.weekly.previous);
+  previousPeriod.textContent = stat.weekly.name;
+  document.addEventListener("change-period", (event: CustomEvent) => {
+   currentValue.textContent = String(stat[event.detail].current);
+   previousValue.textContent = String(stat[event.detail].previous);
+   previousPeriod.textContent = stat[event.detail].name;
+  });
+  grid.appendChild(document.createComment(stat.name));
+  grid.appendChild(statFragment);
+});
