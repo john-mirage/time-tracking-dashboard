a9b768085bbbe545b162e4403ff7cdbb2fb6bd2b
john-mirage <chaosdynamix@posteo.net>
2022-05-02 12:40:01 +0200

    feat: rework buttons

--- src/data/stats.ts
+++ src/data/stats.ts
@@ -8,43 +8,103 @@ import selfCareIcon from "@images/icon-self-care.svg";
 const stats = [
   {
     name: "Work",
-    current: 32,
-    previous: 36,
+    daily: {
+      current: 7,
+      previous: 6,
+    },
+    weekly: {
+      current: 32,
+      previous: 36,
+    },
+    monthly: {
+      current: 120,
+      previous: 125,
+    },
     color: "work",
     image: workIcon,
   },
   {
     name: "Play",
-    current: 10,
-    previous: 8,
+    daily: {
+      current: 1,
+      previous: 2,
+    },
+    weekly: {
+      current: 10,
+      previous: 8,
+    },
+    monthly: {
+      current: 30,
+      previous: 42,
+    },
     color: "play",
     image: playIcon,
   },
   {
     name: "Study",
-    current: 4,
-    previous: 7,
+    daily: {
+      current: 1,
+      previous: 2,
+    },
+    weekly: {
+      current: 4,
+      previous: 7,
+    },
+    monthly: {
+      current: 12,
+      previous: 16,
+    },
     color: "study",
     image: studyIcon,
   },
   {
     name: "Exercise",
-    current: 4,
-    previous: 5,
+    daily: {
+      current: 1,
+      previous: 2,
+    },
+    weekly: {
+      current: 4,
+      previous: 5,
+    },
+    monthly: {
+      current: 16,
+      previous: 20,
+    },
     color: "exercise",
     image: exerciseIcon,
   },
   {
     name: "Social",
-    current: 5,
-    previous: 10,
+    daily: {
+      current: 1,
+      previous: 2,
+    },
+    weekly: {
+      current: 5,
+      previous: 10,
+    },
+    monthly: {
+      current: 20,
+      previous: 21,
+    },
     color: "social",
     image: socialIcon,
   },
   {
     name: "Self Care",
-    current: 2,
-    previous: 2,
+    daily: {
+      current: 0,
+      previous: 1,
+    },
+    weekly: {
+      current: 2,
+      previous: 2,
+    },
+    monthly: {
+      current: 8,
+      previous: 9,
+    },
     color: "self-care",
     image: selfCareIcon,
   },
